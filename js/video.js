d7e3efaf1c8c4608d928f5e0f08faaeedd0b42a2
AllisonDu <>
2023-03-29 21:56:02 -0400

    first

--- js/video.js
+++ js/video.js
@@ -1,11 +1,89 @@
 var video;
+var vol;
+
+var slider = document.getElementById("slider");
+var output = document.getElementById("volume");
+var mute = document.getElementById("mute");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
+	//console.log("Good job opening the window")
+	video = document.querySelector('.video');
+	video.autoplay = true;
+	video.loop = false;
+	//console.log(document.querySelector('.video').autoplay);
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+ 	
+	video.play();
+	//update volume info
+	video.volume = (slider.value)/100;
+	console.log(video.volume);
+ });
+
+ document.querySelector("#pause").addEventListener("click", function() {
+	//console.log("Pause Video");
+    video.pause();
+});
+document.querySelector("#slower").addEventListener("click", function() {
+	//console.log("slower Video");
+	video.playbackRate = video.playbackRate*0.9;
+	console.log(video.playbackRate);
+
+});
+
+
+document.querySelector("#faster").addEventListener("click", function() {
+	//console.log("faster Video");
+	video.playbackRate = video.playbackRate*1.1;
+	console.log(video.playbackRate);
+
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	if(video.currentTime + 10 > video.duration){
+		video.currentTime = 0;
+		console.log(video.currentTime);
+	}
+	else{
+		video.currentTime = video.currentTime + 10;
+		console.log(video.currentTime);
+	}
+	
+});
+
+
+document.querySelector("#mute").addEventListener("click", function() {
+	console.log(video.muted);
+	if(video.muted == false){
+		mute.innerHTML = "Unmute"
+	}
+	else{
+		mute.innerHTML = "Mute"
+	}
+	
+	video.muted = !video.muted;
+
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+
+	video.classList.add("oldSchool");
+});
+document.querySelector("#orig").addEventListener("click", function() {
+
+	video.classList.remove("oldSchool");
+});
+
+
+
+
 
